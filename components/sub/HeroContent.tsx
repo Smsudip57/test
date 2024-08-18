be89669fb7b5031a535e0a6aa184ff3062c1f695
Smsudip57 <sudip7981@gmail.com>
2024-08-19 01:42:34 +0600

    sdf

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -30,7 +30,7 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-col lg:flex-row items-center justify-center px-10 xl:px-20 mt-40  z-[20]"
+      className="flex flex-col lg:flex-row items-center justify-center px-10 xl:px-20 min-h-screen z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start ">
         <motion.div
@@ -72,7 +72,7 @@ const HeroContent = () => {
           height={650}
           width={650}
         /> */}
-        <div className="w-full md:w-full 2xl:w-[60%]  relative block py-36">
+        <div className="w-full md:w-full relative block py-36">
   <div className="mt-10">
     <div className="flex justify-center gap-2 sm:gap-5 mb-4">
       <a href="modern-workplace">
@@ -89,7 +89,7 @@ const HeroContent = () => {
       <img alt="seo" loading="lazy" width="149" height="152" decoding="async" data-nimg="1" className="rounded-full cursor-pointer moving-item-5" style={{color:"transparent",width:imageStyle < 640 ? '100px':'149px', height:imageStyle < 640 ? '100px':'140px'}} src="/Website Development.jpeg"/></a>
     </div>
   </div>
-  <div className="rounded-[70px] w-full border-[1.5px] gap-2 xs:gap-4 flex md:gap-5 border-[#0B2B20] p-1 justify-between bg-white border-box">
+  <div className="rounded-[70px] w-full 2xl:w-[60%] mx-auto border-[1.5px] gap-2 xs:gap-4 flex md:gap-5 border-[#0B2B20] p-1 justify-between bg-white border-box">
     <div className="flex gap-2.5 items-center">
       <img alt="search" loading="lazy" width="25" height="25" decoding="async" data-nimg="1" className="sm:ml-2 xs:w-6 xs:h-6 w-5 h-5" style={{color:"transparent"}} src="/search.svg"/>
       <input placeholder="IT Solutions Galaxy" className="text-[#101513] text-xs xs:text-base leading-7 focus:outline-none"/>
