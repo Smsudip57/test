5efae669dbc7190affa7bb5d0a3078a03c9d1818
Smsudip57 <sudip7981@gmail.com>
2024-08-19 02:06:11 +0600

    sdf

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -89,7 +89,7 @@ const HeroContent = () => {
       <img alt="seo" loading="lazy" width="149" height="152" decoding="async" data-nimg="1" className="rounded-full cursor-pointer moving-item-5" style={{color:"transparent",width:imageStyle < 640 ? '100px':'149px', height:imageStyle < 640 ? '100px':'140px'}} src="/Website Development.jpeg"/></a>
     </div>
   </div>
-  <div className="rounded-[70px] w-full 2xl:w-[60%] mx-auto border-[1.5px] gap-2 xs:gap-4 flex md:gap-5 border-[#0B2B20] p-1 justify-between bg-white border-box">
+  <div className="rounded-[70px] w-full  mx-auto border-[1.5px] gap-2 xs:gap-4 flex md:gap-5 border-[#0B2B20] p-1 justify-between bg-white border-box">
     <div className="flex gap-2.5 items-center">
       <img alt="search" loading="lazy" width="25" height="25" decoding="async" data-nimg="1" className="sm:ml-2 xs:w-6 xs:h-6 w-5 h-5" style={{color:"transparent"}} src="/search.svg"/>
       <input placeholder="IT Solutions Galaxy" className="text-[#101513] text-base xs:text-base leading-7 focus:outline-none"/>
