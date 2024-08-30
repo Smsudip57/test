794850e6839a2ab6aa733550890fa7b0d6f52e8d
Sudip Mandal <sudip7981@gmail.com>
2024-08-30 12:46:45 +0600

    sdf

--- app/customer-success-stories/page.tsx
+++ app/customer-success-stories/page.tsx
@@ -1,11 +1,277 @@
 import React from 'react'
 import Testimonial from '@/components/main/Testimonial'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 export default function page() {
   return (
-    <div className=''>
-        <div className='w-4/5 mx-auto'>
-        <Testimonial />
+    <div className='my-16'>
+        <div className='w-[1280px] mx-auto pt-16 flex items-center gap-10'>
+          <div className='basis-1/2 pr-32'>
+          <p className='text-5xl'>
+            <strong>WEBME delivers Customer Success.
+            </strong>
+            </p>
+          <p className='mt-10 text-xl'>
+          At WEBME, we are dedicated to driving your success through innovative IT services. Our expertise spans ERP, Modern Workplace solutions, Network Security, Website development, and Digital Technology. WEBME acts as your virtual companion in the digital world, helping you achieve success and transform your operations. Partner with us to experience unparalleled customer success.
+          </p>
+          </div>
+          <div className="basis-1/2 z-40 rounded-2xl overflow-hidden">
+            <img alt="logo" loading="lazy" width="100" height="auto" decoding="async" data-nimg="1" className="w-full h-full object-cover" src="/moving-car.gif"/>
+          </div>
+
+
+        </div>
+        <div className='w-[1280px] mx-auto pt-32 gap-10'>
+            <p className='text-4xl w-full text-center'>
+              <strong>
+              Change the Heading to Customer Success with ERP
+              </strong>
+            </p>
+            <div className=' grid grid-cols-4 gap-10 pt-16'>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-Wiley-720x405-1.png?w=1280' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Wiley sees 213% return on investment with Einstein for Service. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Learn how the publisher implemented AI-powered service to cut costs and boost productivity. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-Iron-Mountain-720x405-1-1.png?w=1280' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Iron Mountain achieves peak productivity with Einstein AI 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Learn how generative AI suggests replies and articles to help agents resolve more cases. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-BP-720x405-1.png?w=1280' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  bp boosts service agent productivity with Einstein chat summaries. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                bp agents swap call transcripts for Einstein chat summaries, saving an estimated 2.5-4K hours. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-Simplyhealth-720x405-1.png?w=1280' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Simplyhealth cuts email response times by up to 90% to transform customer support. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Generative AI creates email replies for FAQs to give agents more time for complex work 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+
+          </div>
+
+        </div>
+        <div className='w-[1280px] mx-auto pt-32 gap-10'>
+            <p className='text-4xl w-full text-center'>
+              <strong>
+              Customer Success with ModernWorkplace
+              </strong>
+            </p>
+            <div className=' grid grid-cols-3 gap-10 pt-16'>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/08/Aston-Martin-Card.png?w=1280' className='w-full'/>
+                <div className='p-10 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-2xl mt-4'>
+                  <strong>
+                  Aston Martin delivers luxury experiences powered by Data Cloud. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Learn how Aston Martin unifies customer data for personalized luxury experiences and a 52% reduction in duplicates.  
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-Air-India-720x405-1.png?w=1280' className='w-full'/>
+                <div className='p-10 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Air India delivers effortless, world-class service with Einstein. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Learn how the airline connects data to enhance generative AI and improve agent productivity. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-Turtle-Bay-720x405-2.png?w=1280' className='w-full'/>
+                <div className='p-10 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Turtle Bay Resort elevates hospitality with AI-driven personalization. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Learn how AI-generated service replies and recommendations increase repeat bookings by 20%. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+          </div>
+
+        </div>
+        <div className='w-[1280px] mx-auto pt-32 gap-10'>
+            <p className='text-4xl w-full text-center'>
+              <strong>
+              Customer Success with Workfrom Anywhere
+              </strong>
+            </p>
+            <div className=' grid grid-cols-4 gap-10 pt-16'>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/07/production-resource-card-World-Economic-Forum-720x405-1.png?w=1280' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Connected data enables leaders and programs at World Economic Forum. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                The Forum integrates 64 systems with near zero downtime for improved constituent experiences. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/08/production-resource-card-RBC-720x405-1.png?resize=300,169' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  RBC Wealth Management wows new clients with onboarding in 24 minutes. I 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                RBC Wealth Management consolidated 26 systems into one to exceed client and advisor expectations. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/08/production-resource-card-UberEats-720x405-1.png?resize=300,169' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  Wrong order? Uber Eats resolves cases faster by automating workflows. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                Uber Eats unifies merchant data from order details to payment systems to resolve disputes faster.
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+            <div className='bg-white overflow-hidden rounded-xl shadow-xl flex flex-col'>
+                <img src='https://wp.salesforce.com/en-us/wp-content/uploads/sites/4/2024/08/production-resource-card-Vonage-720x405-1.png?resize=300,169' className='w-full'/>
+                <div className='p-8 h-full relative'>
+                <p className='text-sm bg-[#6a949221] px-3 py-1 rounded-full w-max'>
+                Customer Story
+                </p>
+                <p className='text-xl mt-4'>
+                  <strong>
+                  From 4 days to 4 minutes—how Vonage uses automation to respond faster. 
+                  </strong>
+                </p>
+                <p className='mt-4 mb-14'>
+                See how one telecommunications company achieved massive scale – by simply working smarter. 
+                </p>
+                <button className='absolute bottom-0 mb-8  mt-10 text-[#446E6D] flex items-center rounded font-semibold cursor-pointe'>
+                  <span className='font-bold'>Learn more</span>
+                </button>
+
+                </div>
+              </div>
+
+          </div>
+
         </div>
     </div>
   )
