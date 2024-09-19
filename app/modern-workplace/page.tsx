e6ffede9a88d49dc4c8ef32a06936775c1b4a549
Sudip Mandal <sudip7981@gmail.com>
2024-09-20 01:31:36 +0600

    df

--- app/modern-workplace/page.tsx
+++ app/modern-workplace/page.tsx
@@ -145,35 +145,26 @@ export default function Work() {
             data:[
                 {
                     id:0,
-                    image:'/onet.jpg',
-                    title:'Oracle NetSuite',
+                    image:'/nnetwork.jpg',
+                    title:'Fortify Your Digital Fortress with Our Network Security Solutions',
                     description:[
-                        "Comprehensive Cloud Solution: All-in-one cloud ERP solution that integrates financials, CRM, e-commerce, and more.",
-                        "Scalability: Easily scalable to grow with your business, supporting multiple subsidiaries, currencies, and languages.",
-                        "Real-Time Visibility: Provides real-time dashboards and analytics for better decision-making.",
-                        "Automation: Automates key business processes to improve efficiency and reduce manual errors."
+                        "Stay one step ahead of cyber threats with our cutting-edge network security services. ||From advanced firewall management to real-time intrusion detection and thorough vulnerability assessments, we ensure your digital assets are impenetrable. Protect your business with our robust, proactive security measures and enjoy peace of mind knowing your network is secure."
                     ]
                 },
                 {
                     id:1,
-                    image:'/of.jpg',
-                    title:'Oracle Fusion Cloud ERP',
+                    image:'/nnetwork.jpg',
+                    title:'Managed Firewall Services',
                     description:[
-                        "AI and Machine Learning: Leverages AI and machine learning to automate routine tasks and provide predictive insights.",
-                        "Integrated Suite: Comprehensive suite covering financial management, procurement, project management, and more.",
-                        "Continuous Innovation: Regular updates and new features to keep your business ahead of the curve.",
-                        "User-Friendly Interface: Modern, intuitive interface designed for ease of use and productivity."
+                        "Protect your network with our Managed Firewall Services, offering comprehensive firewall management tailored to your business needs.|| Our experts handle everything from initial setup and configuration to ongoing monitoring and maintenance. We ensure your firewall is always up-to-date with the latest security patches and configurations, providing robust protection against unauthorized access and cyber attacks. Enjoy peace of mind knowing your network perimeter is fortified by industry-leading security measures."
                     ]
                 },
                 {
                     id:2,
-                    image:'/ojd.jpg',
-                    title:'Oracle JD Edwards EnterpriseOne',
+                    image:'/nnetwork.jpg',
+                    title:'Vulnerability Assessment & Penetration Testing',
                     description:[
-                        "Flexible Deployment: Available on-premise, in the cloud, or as a hybrid solution.",
-                        "Industry-Specific Solutions: Tailored solutions for various industries, including manufacturing, construction, and distribution.",
-                        "Mobile Capabilities: Mobile applications for on-the-go access to critical business data.",
-                        "Robust Analytics: Advanced analytics and reporting tools for in-depth business insights."
+                        "Ensure your network’s defenses are impenetrable with our Vulnerability Assessment & Penetration Testing services.|| Our experts conduct thorough assessments to identify potential weaknesses in your system, followed by simulated cyber-attacks to test your defenses. This proactive approach helps uncover vulnerabilities before they can be exploited, providing you with detailed reports and actionable recommendations to strengthen your security posture."
                     ]
                 },
             ]
@@ -254,7 +245,7 @@ export default function Work() {
     <div className=' w-[1280px] mx-auto flex '>
             <div className='w-[50%] flex flex-col justify-center gap-10 z-30'>
                 <strong className='text-[44px] font-bold leading-[52px] font-sans'>Lead the Digital Era with State-of-the-Art Technology</strong>
-                <p className='pr-10 font-medium'>In the complex world of business management, simplicity is the ultimate sophistication.<br></br><br></br>  ERP software—a game-changer that integrates every aspect of your operations into a single, seamless platform. In the constellation of business tools, ERP software shines as the supernova of operational excellence. It’s not just a system; it’s the nucleus of your enterprise, orchestrating a symphony of streamlined processes and integrated data.</p>
+                <p className='pr-10 font-medium'>In the complex world of business management, simplicity is the ultimate sophistication.<br></br><br></br>  Elevate your business with our top-tier Network Security and ERP Software services. Shield your digital assets with our advanced network security solutions, featuring state-of-the-art firewall management, real-time intrusion detection, and comprehensive vulnerability assessments. Simultaneously, revolutionize your operations with our ERP software, seamlessly integrating finance, HR, and supply chain into one powerful system. Experience unparalleled efficiency, enhanced decision-making, and robust growth with our tailored solutions designed for businesses of all sizes.</p>
                 <div className='flex gap-6'>
                     <button className='align-start bg-[#446E6D] text-[#fff] px-4 py-2 rounded  hover:opacity-70 text-sm'>Book Free Consultation</button>
                     <button className='align-start hover:bg-[#00000028] text-black px-4 py-2 rounded hover:text-white text-base'><span className='mr-1'>Explore</span> <EastIcon fontSize='inherit '/></button>
