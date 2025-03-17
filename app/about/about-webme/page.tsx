4ef8daa6568fe8874ea379deaccd8b8fb536fe59
Sudip Mandal <sudip7981@gmail.com>
2025-03-17 20:03:40 +0600

    as

--- app/about/about-webme/page.tsx
+++ app/about/about-webme/page.tsx
@@ -29,11 +29,82 @@ export default function page() {
             <div className="mt-16 lg:mt-10 flex flex-col lg:pr-28">
               <span className="text-[#446E6D] mb-2">About</span>
               <span className="text-5xl mb-7">WEBME</span>
-              <p className="text-gray-500 whitespace-pre-line">
-                {
-                  "WEBME: Your Co-Pilot in the IT Journey\n\nWith a passion for technology and a commitment to excellence, WEBME is here to guide you through every step of your IT journey, ensuring seamless operations and transformative growth. We believe that technology should be an enabler, not a hindrance, and that's why we strive to deliver tailor-made IT solutions that align with your unique needs.\n\nWEBME Information Technology is here to guide you through the complex landscape of IT, ensuring that your technology supports your goals and drives success. WEBME, your virtual companion in the digital world and your trusted IT consultancy provider.\n\nWEBMEDIGITAL: Your IT Solutions Galaxy\n\nOnce you connect with WEBME, you'll have an all-in-one solution WEBMEDIGITAL at your fingertips, eliminating the need to rely on any other service and allowing you to focus entirely on growing. Whether you're looking to enhance your digital presence, streamline your operations, or ensure robust support, we've got you covered.\n\nWork From Anywhere: Empower your team with the flexibility to work from anywhere, ensuring productivity and collaboration no matter where they are.\n\nExpertise: Leverage our deep industry knowledge and technical expertise to drive your business forward.\n\nBranding: Strengthen your brand with our innovative IT solutions that enhance your digital footprint and market presence.\n\nModern Workplace: Transform your workplace with cutting-edge technology that fosters innovation and efficiency.\n\nEndless Support: Enjoy peace of mind with our continuous support, ensuring your IT infrastructure is always running smoothly.\n\nDigital: Embrace the digital age with our comprehensive services that cover all aspects of your IT needs.\n\nReady to transform your IT journey? Connect with WEBME and become part of a community dedicated to a future of excellence, innovation, and the freedom to focus on what matters most."
-                }
-              </p>
+              <div className="text-gray-500">
+                <p className="font-bold text-lg mb-2">
+                  WEBME: Your Co-Pilot in the IT Journey
+                </p>
+                <p className="mb-4">
+                  With a passion for technology and a commitment to excellence,
+                  WEBME is here to guide you through every step of your IT
+                  journey, ensuring seamless operations and transformative
+                  growth. We believe that technology should be an enabler, not a
+                  hindrance, and that's why we strive to deliver tailor-made IT
+                  solutions that align with your unique needs.
+                </p>
+
+                <p className="mb-4">
+                  WEBME Information Technology is here to guide you through the
+                  complex landscape of IT, ensuring that your technology
+                  supports your goals and drives success. WEBME, your virtual
+                  companion in the digital world and your trusted IT consultancy
+                  provider.
+                </p>
+
+                <p className="font-bold text-lg mb-2 mt-6">
+                  WEBMEDIGITAL: Your IT Solutions Galaxy
+                </p>
+                <p className="mb-4">
+                  Once you connect with WEBME, you'll have an all-in-one
+                  solution WEBMEDIGITAL at your fingertips, eliminating the need
+                  to rely on any other service and allowing you to focus
+                  entirely on growing. Whether you're looking to enhance your
+                  digital presence, streamline your operations, or ensure robust
+                  support, we've got you covered.
+                </p>
+
+                <p className="mb-2 mt-4">
+                  <span className="font-bold">Work From Anywhere:</span> Empower
+                  your team with the flexibility to work from anywhere, ensuring
+                  productivity and collaboration no matter where they are.
+                </p>
+
+                <p className="mb-2">
+                  <span className="font-bold">Expertise:</span> Leverage our
+                  deep industry knowledge and technical expertise to drive your
+                  business forward.
+                </p>
+
+                <p className="mb-2">
+                  <span className="font-bold">Branding:</span> Strengthen your
+                  brand with our innovative IT solutions that enhance your
+                  digital footprint and market presence.
+                </p>
+
+                <p className="mb-2">
+                  <span className="font-bold">Modern Workplace:</span> Transform
+                  your workplace with cutting-edge technology that fosters
+                  innovation and efficiency.
+                </p>
+
+                <p className="mb-2">
+                  <span className="font-bold">Endless Support:</span> Enjoy
+                  peace of mind with our continuous support, ensuring your IT
+                  infrastructure is always running smoothly.
+                </p>
+
+                <p className="mb-2">
+                  <span className="font-bold">Digital:</span> Embrace the
+                  digital age with our comprehensive services that cover all
+                  aspects of your IT needs.
+                </p>
+
+                <p className="mt-4">
+                  Ready to transform your IT journey? Connect with WEBME and
+                  become part of a community dedicated to a future of
+                  excellence, innovation, and the freedom to focus on what
+                  matters most.
+                </p>
+              </div>
               <button className="bg-[#446E6D] text-white py-2 px-4 flex items-center rounded font-semibold gap-2 cursor-pointer w-max mt-7">
                 <span>
                   Contact us <OpenInNewIcon fontSize="inherit" />
