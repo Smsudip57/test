7c9818cc30a0b44f59dd27d875efc554dfcce31f
Sudip Mandal <sudip7981@gmail.com>
2025-03-17 19:53:05 +0600

    sd

--- app/about/about-webme/page.tsx
+++ app/about/about-webme/page.tsx
@@ -3,7 +3,7 @@ import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 export default function page() {
   return (
-    <div className="min-h-screen  pt-20 xl:pt-0">
+    <div className="min-h-screen  py-[100px] xl:pt-0">
       <div className="mx-auto mb-20 lg:mb-0 lg:h-[680px] bg-white max-w-[1536px] flex  items-center ">
         <div className=" flex flex-col lg:flex-row gap-6">
           <div className="lg:basis-1/2 z-10 relative">
