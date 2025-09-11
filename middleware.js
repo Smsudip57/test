24718bb52295e8775b365192987c5d2cec120c8b
sudip-codencoit <sudip.mandal@codencoit.com>
2025-09-11 08:08:54 +0600

    refactor: remove development mode check from middleware function

--- middleware.js
+++ middleware.js
@@ -1,9 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  if (process.env.NODE_ENV === "development") {
-    return NextResponse.next();
-  }
 
   const { pathname } = request.nextUrl;
   console.log("Middleware running for:", pathname);
